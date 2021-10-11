9b8ac317f5d611aeb589123fc23ecd43f6f30795
tyler <tylercobb92@gmail.com>
2021-10-11 14:07:17 -0400

    search giphy button successfully gets response from Giphy API, resets search field

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("Let's get this party started!");
+const $search = $('#searchTerm')
+
+
+$('form').on('submit', async function (e) {
+    e.preventDefault();
+
+    let searchTerm = $search.val();
+    $search.val('');
+
+    const response = await axios.get('https://api.giphy.com/v1/gifs/search', {
+        params: {
+            q: searchTerm,
+            api_key: 'lMBjWCtuNP1Iq5tXNAZLbnJP5QbSI09w'
+        }
+    });
+    console.log(response);
+})
